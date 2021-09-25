e3df49da81a5784e6c3b53dd5dfec82cfb172e50
Roberto Allende <rallende@gmail.com>
2021-09-25 20:40:40 +1200

    Add A common ground for amateur football. Improve about. Add analytics.

--- football/assets/search.js
+++ football/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/football/2018/06/01/header-image.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"A common ground for amateur football",'url':"/football/2021/09/25/a-common-ground-for-amateur-football.html"}]};
